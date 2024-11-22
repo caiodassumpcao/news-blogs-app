e9a07492c3bced5f21f9f5a4e797b43b6075a250
caiodassumpcao <caio.rodri.d@gmail.com>
2024-11-22 12:28:49 -0300

    Costumizing Navbar and User

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Weather from './Weather'
 import Calendar from './Calendar'
 import './News.css'
+import userImg from '../assets/user.jpg'
+
 
 const News = () => {
   return (
@@ -21,8 +23,28 @@ const News = () => {
       <div className="news-content">
 
         <div className="navbar">
-          <div className="user">User</div>
-          <nav className="categories">Categories</nav>
+          <div className="user">
+            <img src={userImg} alt="User Image" />
+            <p>Mary's Blog</p>
+          </div>
+          <nav className="categories">
+            <h1 className="nav-heading">Categories</h1>
+            <div className="nav-links">
+              <a href="#" className='nav-link'>General</a>
+              <a href="#" className='nav-link'>World</a>
+              <a href="#" className='nav-link'>Business</a>
+              <a href="#" className='nav-link'>Technology</a>
+              <a href="#" className='nav-link'>Entertainment</a>
+              <a href="#" className='nav-link'>Sports</a>
+              <a href="#" className='nav-link'>Science</a>
+              <a href="#" className='nav-link'>Health</a>
+              <a href="#" className='nav-link'>Nation </a>
+              <a href="#" className='nav-link'>
+                Bookmarks <i className="fa-regular 
+                fa-bookmark"></i>
+              </a>
+            </div>
+          </nav>
         </div>
         
         <div className="news-section">
