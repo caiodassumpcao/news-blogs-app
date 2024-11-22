6844867c6e05475a331821d085d13b57c9158f61
caiodassumpcao <caio.rodri.d@gmail.com>
2024-11-22 15:55:16 -0300

    Integrate API - part 2

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -2,33 +2,53 @@ import React, { useEffect, useState } from 'react'
 import Weather from './Weather'
 import Calendar from './Calendar'
 import './News.css'
-import userImg from '../assets/user.jpg'
-import techImg from '../assets/tech.jpg'
-import sportsImg from '../assets/sports.jpg'
-import scienceImg from '../assets/science.jpg'
-import worldImg from '../assets/world.jpg'
-import healthImg from '../assets/health.jpg'
-import nationImg from '../assets/nation.jpg'
+import userImg from '../assets/perfil.jpg'
+import noImg from '../assets/no-img.png'
 import axios from 'axios'
+
+const categories = [
+  "general", 
+  "world", 
+  "business", 
+  "technology", 
+  "entertainment", 
+  "sports", 
+  "science", 
+  "health", 
+  "nation", 
+]
 
 const News = () => {
   const [headline, setHeadline] = useState(null)
   const [news, setNews] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('general') 
 
   useEffect(() => {
     const fetchNews = async () => {
-      const url = 'https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=4c1fba9049f63a580a86a4177e57a188'
+      const url = 'https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey=4c1fba9049f63a580a86a4177e57a188'
     
       const response = await axios.get(url)
       const fetchedNews = response.data.articles
 
-      setHeadline(fetchedNews[0])
+      fetchedNews.forEach((article) => {
+        if (!article.image) {
+          article.image = noImg
+        }
+      })
 
-      console.log(fetchedNews[0])
+      setHeadline(fetchedNews[0])
+      setNews(fetchedNews.slice(1, 7))
+
+      console.log(news)
     }
 
     fetchNews()
-  }, [])
+  }, [selectedCategory])
+
+  const handleCategoryClick = (e, category) => {
+    e.preventDefault()
+    setSelectedCategory(category)
+  }
 
   return (
     <div className="news">
@@ -49,20 +69,21 @@ const News = () => {
         <div className="navbar">
           <div className="user">
             <img src={userImg} alt="User Image" />
-            <p>Mary's Blog</p>
+            <p>Caio's Blog</p>
           </div>
           <nav className="categories">
             <h1 className="nav-heading">Categories</h1>
             <div className="nav-links">
-              <a href="#" className='nav-link'>General</a>
-              <a href="#" className='nav-link'>World</a>
-              <a href="#" className='nav-link'>Business</a>
-              <a href="#" className='nav-link'>Technology</a>
-              <a href="#" className='nav-link'>Entertainment</a>
-              <a href="#" className='nav-link'>Sports</a>
-              <a href="#" className='nav-link'>Science</a>
-              <a href="#" className='nav-link'>Health</a>
-              <a href="#" className='nav-link'>Nation </a>
+              {categories.map((category) => (
+                <a 
+                  href="#" 
+                  key={category} 
+                  className='nav-link'
+                  onClick={(e) => handleCategoryClick(e, category)} 
+                >
+                  {category}
+                </a>
+              ))}
               <a href="#" className='nav-link'>
                 Bookmarks <i className="fa-regular 
                 fa-bookmark"></i>
@@ -74,7 +95,7 @@ const News = () => {
         <div className="news-section">
           {headline && (          
             <div className="headline">
-              <img src={headline.image} alt={headline.title} />
+              <img src={headline.image || noImg} alt={headline.title} />
               <h2 className='headline-title'>
                 {headline.title}
                 <i className='fa-regular fa-bookmark bookmark'></i>
@@ -83,51 +104,17 @@ const News = () => {
           )}
 
           <div className="news-grid">
-            <div className="news-grid-item">
-              <img src={techImg} alt="News Image" />
-              <h3>
-                Lorem ipsum dolor sit amet.
-                <i className='fa-regular fa-bookmark bookmark'></i>
-              </h3>
-            </div>
-            <div className="news-grid-item">
-              <img src={sportsImg} alt="News Image" />
-              <h3>
-                Lorem ipsum dolor sit amet.
-                <i className='fa-regular fa-bookmark bookmark'></i>
-              </h3>
-            </div>
-            <div className="news-grid-item">
-              <img src={scienceImg} alt="News Image" />
-              <h3>
-                Lorem ipsum dolor sit amet.
-                <i className='fa-regular fa-bookmark bookmark'></i>
-              </h3>
-            </div>
-            <div className="news-grid-item">
-              <img src={worldImg} alt="News Image" />
-              <h3>
-                Lorem ipsum dolor sit amet.
-                <i className='fa-regular fa-bookmark bookmark'></i>
-              </h3>
-            </div>
-            <div className="news-grid-item">
-              <img src={healthImg} alt="News Image" />
-              <h3>
-                Lorem ipsum dolor sit amet.
-                <i className='fa-regular fa-bookmark bookmark'></i>
-              </h3>
-            </div>
-            <div className="news-grid-item">
-              <img src={nationImg} alt="News Image" />
-              <h3>
-                Lorem ipsum dolor sit amet.
-                <i className='fa-regular fa-bookmark bookmark'></i>
-              </h3>
-            </div>
+            {news.map((article, index) => (            
+              <div key={index} className="news-grid-item">
+                <img src={article.image || noImg} alt={article.title} />
+                <h3>
+                  {article.title}
+                  <i className='fa-regular fa-bookmark bookmark'></i>
+                </h3>
+              </div>
+            ))}
           </div>
         </div>
-        
         <div className="my-blogs">My Blogs</div>
         <div className="weather-calendar">
           <Weather />
