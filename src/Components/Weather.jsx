dab7cff8004ecf77215eb27a0097213f60339c43
caiodassumpcao <caio.rodri.d@gmail.com>
2024-11-25 15:03:25 -0300

    Creating JSX for weather Gadget

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 import './Weather.css'
 
 const Weather = () => {
-  return <div className='weather'>Weather</div>
+  return <div className='weather'>
+    <div className="search">
+      <div className="search-top">
+        <i className="fa-solid fa-location-dot"></i>
+        <div className="location">Tbilisi</div>
+      </div>
+      <div className="search-location">
+        <input type="text" placeholder='Enter Location' />
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+    <div className="weather-data">
+      <i class='bx bxs-sun'></i>
+      <div className="weather-type">Clear</div>
+      <div className="temp">28°</div>
+    </div>
+  </div>
 }
 
 export default Weather
