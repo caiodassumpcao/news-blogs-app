6e5bd191b4fab7d60960dff60598b1fd3df8febb
caiodassumpcao <caio.rodri.d@gmail.com>
2024-11-27 13:51:37 -0300

    Navigation betsween Sections

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -20,7 +20,7 @@ const categories = [
   "nation", 
 ]
 
-const News = () => {
+const News = ({onShowBlogs}) => {
   const [headline, setHeadline] = useState(null)
   const [news, setNews] = useState([])
   const [selectedCategory, setSelectedCategory] = useState('general') 
@@ -105,7 +105,7 @@ const News = () => {
       <div className="news-content">
 
         <div className="navbar">
-          <div className="user">
+          <div className="user" onClick={onShowBlogs}>
             <img src={userImg} alt="User Image" />
             <p>Caio's Blog</p>
           </div>
